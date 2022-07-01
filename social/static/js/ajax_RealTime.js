c2fe9da02e838340f37ad392b3ee677d80a2e49b
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-07-02 01:54:23 +0200

    Update ajax_RealTime.js

--- social/static/js/ajax_RealTime.js
+++ social/static/js/ajax_RealTime.js
@@ -11,24 +11,24 @@ $('document').ready(function () {
             success: function (notification_bar_len) {
                 var notification_bar_len = notification_bar_len['notification_bar_len']
                 elementInbox = document.getElementById('notifications-inbox');
-                // elementInbox.style = "display: block; padding-left: 1000px;";
-                elementInbox.innerHTML = notification_bar_len;
 
 
                 if(notification_bar_len > 99) {
-                    elementInbox.style = "display: inline-block; font-size: 12px; padding: 2px; padding-left: 3px;";
+                    elementInbox.style = "display: inline-block; font-size: 12px; padding: 2px; padding-left: 3px; font-wight: bold;";
                     elementInbox.innerHTML = 99;
                 }
-                else if(notification_bar_len > 9 && notification_bar_len <= 99) {
-                    elementInbox.style = "display: inline-block; font-size: 12px; padding: 2px; padding-left: 3px;";
-                    elementInbox.innerHTML = notification_bar_len;
-                }
+                // else if(notification_bar_len > 9 && notification_bar_len <= 99) {
+                //     elementInbox.style = "display: inline-block; font-size: 12px; padding: 2px; padding-left: 3px;";
+                //     elementInbox.innerHTML = notification_bar_len;
+                // }
                 else if(notification_bar_len == 0){
                     elementInbox.style = "display: none;"
                 }
                  else {
                     elementInbox.removeAttribute("style");
-                    elementInbox.style = "display: inline-block; padding-left: 5.2px;";
+                    elementInbox.style = "display: inline-block; font-size: 12px; padding: 2px; padding-left: 6px; font-wight: bold;";
+                    elementInbox.innerHTML = notification_bar_len;
+
                 }
                 
             }
