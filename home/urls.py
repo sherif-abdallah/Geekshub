c51db4563162bc91dcd6388f8ee3a7f273dd5bb7
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-07-01 22:45:23 +0200

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = "home"
+
+urlpatterns = [
+
+    path('', Home_Page_views, name='homepage'),
+    path('search', Search_Page_views, name="search"),
+
+
+
+]
