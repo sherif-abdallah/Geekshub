74433074df5bc346c75363e9061663e51c17ccda
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-07-01 22:43:47 +0200

    Delete posts directory

--- posts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Post_Like)
-admin.site.register(Post_Comments)
-admin.site.register(Post_Favorite)
