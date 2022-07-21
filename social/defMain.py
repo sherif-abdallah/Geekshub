935365b7e45f8e93f9443b369507b14489d5ea52
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-07-22 00:29:23 +0200

    Add files via upload

---
+++ social/defMain.py
@@ -0,0 +1,68 @@
+from accounts.models import Profile_picture
+from inbox.models import Messages_Inbox
+from django.core.paginator import Paginator
+import re, random
+from django.db.models import  Q
+from datetime import datetime, timezone
+from django.contrib.auth.models import User
+
+
+
+# Create your Packages here.
+
+def Last_Messages_Friend_id(request):
+    msgs = Messages_Inbox.objects.filter(Q(to_user_id=request.user.id) | Q(from_user_id=request.user.id)).values('from_user', 'to_user').order_by('-id')
+    if msgs[0]['from_user'] != request.user.id:
+        last_msg_friend_id = msgs[0]['from_user']
+    elif msgs[0]['to_user'] != request.user.id:
+        last_msg_friend_id = msgs[0]['to_user']
+    else:
+        last_msg_friend_id = None
+    
+
+    return last_msg_friend_id
+
+def Message_Notification_views(request):
+    msg_notification_bar = Messages_Inbox.objects.filter(to_user_id=request.user.id, seen=False)
+    msg_notification_bar_len = msg_notification_bar.count()
+    return msg_notification_bar_len
+
+
+def check_email_is_valid(email):  
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex, email)):   
+        return True
+    else:
+        return False
+
+# Check If Company Account didnt send to the user message
+def check_if_company_account_didnt_send_to_user(request):
+    company_account_id = User.objects.get(username="Geekshub").id
+    messages_inbox = Messages_Inbox.objects.filter(to_user_id=request.user.id, from_user=company_account_id)
+    if len(messages_inbox) == 0:
+        msg = f"Hi { request.user.username }, Welcome to Geekshub. Your account has been created – now it will be easier than ever to share and connect with your friends and family. Your new account comes with access to Geekshub products, apps, and services."
+        Messages_Inbox.objects.create(to_user_id=request.user.id, from_user_id=company_account_id, date_utc=datetime.now(timezone.utc).strftime("%Y-%m-%d %I:%M:%S %p"), msg=msg)
+
+# Cheek if user dosent have profile picture
+def cheek_user_picture(request):
+    profile_picture = Profile_picture.objects.filter(author_id=request.user)
+    if len(profile_picture) == 0:
+        Profile_picture.objects.create(author_id=request.user.id)
+
+# Paginate the posts
+def paginate(request, posts, num_posts):
+    paginator = Paginator(posts, num_posts)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return posts
+
+# Reverse the list
+def Reverse(lst):
+    return [ele for ele in reversed(lst)]
+
+# Convert list to string
+def listToStringWithoutBrackets(lst):
+    return str(lst).replace('[','').replace(']','')
+
+
+
---
+++ social/urls.py
@@ -0,0 +1,46 @@
+"""social URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # allauth
+    path('accounts/', include('allauth.urls')),
+
+
+    path('accounts/', include('accounts.urls'), name="accounts"),
+    path('post/', include('posts.urls'), name="posts"),
+    path('', include('home.urls'), name="home"),
+
+    # Notifications
+    path('notifications/', include('notifications.urls'), name="notifications"),
+
+    # Inbox
+    path('messages/', include('inbox.urls'), name="messages"),
+
+    # Reset Password
+    path('reset_password/', PasswordResetView.as_view(template_name="password_reset/reset_password.html"), name="password_reset"),
+    path('reset_password_sent/', PasswordResetDoneView.as_view(template_name="password_reset/reset_sent.html"), name="password_reset_done"),
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_confim.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', PasswordResetCompleteView.as_view(template_name="password_reset/password_reset_complete.html"), name="password_reset_complete"),
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
