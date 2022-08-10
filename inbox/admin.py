e2029f3971fbe46d7a3f40910bc60c877e25f89c
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-08-10 06:10:56 +0200

    Delete inbox directory

--- inbox/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-class Messages_Inbox_Admin(admin.ModelAdmin):
-    readonly_fields = ('date_utc', 'created_at')
-
-
-admin.site.register(Messages_Inbox, Messages_Inbox_Admin)
