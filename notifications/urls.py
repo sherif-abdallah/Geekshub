6370dbd842ecf7db405011bd3f9c424f2090354c
Sherif Abdullah <sherif.abdulla@outlook.com>
2022-08-10 06:11:17 +0200

    Delete notifications directory

--- notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = "notifications"
-
-urlpatterns = [
-        path('', Notification_Page_views , name="inbox"),
-        path('json/', Notification_json_api , name="json"),
-]
